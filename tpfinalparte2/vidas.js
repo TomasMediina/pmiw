cee465ed61fae445d71fc7d970b37c5266d2cfd4
TomasMediina <168316172+TomasMediina@users.noreply.github.com>
2024-11-16 22:01:19 -0300

    Add files via upload
    subidooo

---
+++ tpfinalparte2/vidas.js
@@ -0,0 +1,20 @@
+class Vida {
+  constructor(){
+    this.vida=3;
+  }
+  reducirVida(){
+    this.vida--;
+  }
+  reinicarVida(){
+    this.vida=3;
+  }
+  
+  mostrarVida(){
+ textAlign(LEFT,TOP);
+ textSize(30);
+ fill(255);
+ text("vidas");
+ 
+  }
+}
+  
